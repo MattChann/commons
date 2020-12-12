c1e238b577216ee3c6504d594b58b3ed07a7927b
Matthew Chan <mc2438@cornell.edu>
2020-12-11 21:19:45 -0500

    Backend implemented

--- backend/index.ts
+++ backend/index.ts
@@ -4,7 +4,7 @@ import * as admin from 'firebase-admin';
 const serviceAccount = require('./service-account.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
@@ -12,7 +12,67 @@ const db = admin.firestore();
 const app = express();
 app.use(express.json());
 
+const users = db.collection('users');
 
+
+type User = {
+    name: string,
+    email: string,
+    interests: string[],
+    classes: string[],
+    clubs: string[],
+};
+
+// Create new user
+app.post('/addUser', (req, res) => {
+    const user: User = req.body;
+    const newDoc = users.doc();
+    newDoc.set(user);
+    res.send(newDoc.id);
+});
+
+type UserWithID = User & {
+    id: string,
+};
+
+// Get user info
+app.get('/getUser', async (req, res) => {
+    const id: string = req.query.id as string;
+    const doc = await users.doc(id).get();
+    const user: UserWithID = doc.data() as UserWithID;
+    user.id = doc.id;
+    res.send(user);
+});
+
+// Update a user
+app.post('/updateUser', async (req, res) => {
+    const updatedUser: UserWithID = req.body;
+    await users.doc(updatedUser.id).update(updatedUser as User);
+    res.send(updatedUser.id);
+});
+
+// Get recommended users
+app.post('/getCommon', async (req, res) => {
+    const currUser: UserWithID = req.body;
+    const allDocs = await users.orderBy('name', 'asc').get();
+    const allUsers: UserWithID[] = [];
+    for (const doc of allDocs.docs) {
+        let thisUser: UserWithID = doc.data() as UserWithID;
+        thisUser.id = doc.id;
+        allUsers.push(thisUser);
+    };
+    const commonInterests: UserWithID[] = allUsers.filter(user => user.interests.some(interest => currUser.interests.includes(interest)));
+    const commonClasses: UserWithID[] = allUsers.filter(user => user.classes.some(currClass => currUser.classes.includes(currClass)));
+    const commonClubs: UserWithID[] = allUsers.filter(user => user.clubs.some(club => currUser.clubs.includes(club)));
+    res.send([...commonInterests, ...commonClasses, ...commonClubs].filter(user => user.id !== currUser.id));
+});
+
+// Delete a user
+app.delete('/deleteUser', async (req, res) => {
+    const id: string = req.query.id as string;
+    await users.doc(id).delete();
+    res.send('Successfully deleted user');
+});
 
 
 
